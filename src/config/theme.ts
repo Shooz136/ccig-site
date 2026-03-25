dd6deeda93253618bf511c35a09f3adc32d4396b
Benjamin Shoemaker <ben.shoemaker@themlc.com>
2026-03-24 22:30:03 -0400

    Expand theme font options

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -6,7 +6,27 @@ const fontStacks = {
   inter:
     '"Inter Variable", Inter, ui-sans-serif, system-ui, sans-serif',
   system:
-    'ui-sans-serif, system-ui, sans-serif'
+    'ui-sans-serif, system-ui, sans-serif',
+  helvetica:
+    '"Helvetica Neue", Helvetica, Arial, sans-serif',
+  arial:
+    'Arial, "Helvetica Neue", Helvetica, sans-serif',
+  georgia:
+    'Georgia, "Times New Roman", Times, serif',
+  times:
+    '"Times New Roman", Times, serif',
+  garamond:
+    'Garamond, Baskerville, "Times New Roman", serif',
+  palatino:
+    '"Palatino Linotype", "Book Antiqua", Palatino, serif',
+  trebuchet:
+    '"Trebuchet MS", "Lucida Grande", "Lucida Sans Unicode", sans-serif',
+  verdana:
+    'Verdana, Geneva, sans-serif',
+  tahoma:
+    'Tahoma, Geneva, sans-serif',
+  courier:
+    '"Courier New", Courier, monospace'
 } as const;
 
 export const theme = {
